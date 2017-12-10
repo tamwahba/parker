fe62e887fe33f4c9c1b7ea5f219ddc90c0d0506e
Tamer Wahba <tamwahba@hotmail.com>
2017-12-09 22:21:19 -0500

    switch admin date_ranges to a raw_id_field (for performance reasons)

--- nyc/admin.py
+++ nyc/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import DateRange, ParkingRule
 
+
+class ParkingRuleAdmin(admin.ModelAdmin):
+    raw_id_fields = ('date_ranges', )
+
+
 admin.site.register(DateRange)
-admin.site.register(ParkingRule)
+admin.site.register(ParkingRule, ParkingRuleAdmin)
