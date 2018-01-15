1d1534d85a46d83ed74bf60b19e1c5b1c344cfd5
Tamer Wahba <tamwahba@hotmail.com>
2018-01-14 20:53:00 -0500

    add sorting for results

--- nyc/views.py
+++ nyc/views.py
@@ -1,3 +1,4 @@
+from django.contrib.gis.db.models.functions import Distance
 from django.contrib.gis.geos import Point
 from rest_framework import viewsets
 
@@ -6,12 +7,12 @@ from .serializers import DateRangeSerializer, ParkingRuleSerializer
 
 
 class DateRangeViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = DateRange.objects.all()
+    queryset = DateRange.objects.all().order_by('id')
     serializer_class = DateRangeSerializer
 
 
 class ParkingRuleViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = ParkingRule.objects.all()
+    queryset = ParkingRule.objects.all().order_by('id')
     serializer_class = ParkingRuleSerializer
 
 
@@ -25,5 +26,7 @@ class ParkingRuleByLocationViewSet(viewsets.ReadOnlyModelViewSet):
 
         point = Point(x=longitude, y=latitude)
 
-        return ParkingRule.objects.filter(
-            street_segment__dwithin=(point, radius))
+        return ParkingRule.objects\
+            .filter(street_segment__dwithin=(point, radius))\
+            .annotate(distance=Distance('street_segment', point))\
+            .order_by('distance')
