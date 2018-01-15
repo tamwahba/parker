302204b6958032445779ed5bf353cb9bba1498c6
Tamer Wahba <tamwahba@hotmail.com>
2018-01-14 20:48:05 -0500

    get debug, allowed hosts from environment variables

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 #### Pre-run Checklist ####
 - `pip install -r requirements.txt` (assuming pwd is root of this project)
 - `SECRET_KEY` environment variable (large random string)
+- `DEBUG` environment variable (set to 'true' to enable debug mode)
+- `ALLOWED_HOSTS` environment variable (comma-separated strings)
 - PostGIS (`brew install postgis`) or using instructions at https://postgis.net/install/
 - PostgreSQL instance with environments variables: (required by PostGIS)
   - `DATABASE_NAME`
--- parker/settings.py
+++ parker/settings.py
@@ -19,13 +19,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
